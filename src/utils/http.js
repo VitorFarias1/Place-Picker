4d6d41a408b1f67d1217b1c667cbb9d4a8034b47
vitorfarias1 <vitor.rodriguesfarias@gmail.com>
2026-02-19 17:29:35 -0300

    refactor: create util folder to centralize http requests

---
+++ src/utils/http.js
@@ -0,0 +1,27 @@
+export async function fetchAvailablePlaces() {
+    const response = await fetch('http://localhost:3000/places');
+    const resData = await response.json();
+
+    if (!response.ok) {
+        throw new Error('Could not fetch data! Try again later!');
+    }
+
+    return resData.places;
+}
+
+export async function updateUserPlaces(places) {
+    const response = await fetch('http://localhost:3000/user-paces', {
+        method: 'PUT',
+        body: JSON.stringify({places: places}),
+        headers: {
+            'Content-Type':'application/json'
+        }
+    })
+    const resData = await response.json();
+
+    if (!response.ok) {
+        throw new Error('Error updating data.')
+    }
+
+    return resData.message;
+}
