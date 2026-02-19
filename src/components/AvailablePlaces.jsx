bdc13cc1c4f5c40c082fecfa3f5b331c0d53a3eb
vitorfarias1 <vitor.rodriguesfarias@gmail.com>
2026-02-19 12:51:04 -0300

    wip: create get request to the server and handle error

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,45 @@
+import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
+import ErrorPage from './Error.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, SetAvailablePlaces] = useState([]);
+  const [isFetchingData, setIsFetchingData] = useState(false);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetchingData(true);
+      try {
+        const response = await fetch('http://localhost:3000/placesss');
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error('Could not fetch data! Try again later!');
+        }
+
+        SetAvailablePlaces(resData.places);
+        
+      } catch (error) {
+        setError({message: error.message || 'Something went wrong, please try again later.'})
+      }
+
+      setIsFetchingData(false);
+    }
+
+    fetchPlaces();
+  }, [])
+
+  if (error) {
+    return <ErrorPage title="An error ocurred" message={error.message}/>
+  }
+  
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      isLoading={isFetchingData}
+      loadingText="Fetching some places..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
